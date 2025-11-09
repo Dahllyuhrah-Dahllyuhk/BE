2ea4b9abb1deb7a3782fceb4fc1e282d88ebbfef
Daewon Jeong <daewon6905@gmail.com>
2025-11-09 10:54:33 +0900

    Refactor : 로그인 성공 핸들러 메서드 분리

--- src/main/java/org/dallyeo/matuabom/handler/JwtLoginSuccessHandler.java
+++ src/main/java/org/dallyeo/matuabom/handler/JwtLoginSuccessHandler.java
@@ -24,21 +24,18 @@ import java.util.Map;
 @Component
 @RequiredArgsConstructor
 public class JwtLoginSuccessHandler implements org.springframework.security.web.authentication.AuthenticationSuccessHandler {
-
+    private static final String REDIRECT_URL = "http://localhost:3000/profile";
     private final JwtUtil jwtUtil;
     private final UserService userService;
     private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
 
-        OAuth2AuthorizedClient client = oAuth2AuthorizedClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), oauthToken.getName());
-        Map<String, Object> attributes =oauth2User.getAttributes();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        KakaoUserInfo userInfo = objectMapper.convertValue(attributes, KakaoUserInfo.class);
+        OAuth2AuthorizedClient client = getOAuth2AuthorizedClient(oauthToken);
+        KakaoUserInfo userInfo = getKakaoUserInfo(authentication);
 
         Long kakaoId = userInfo.getId();
         String nickname = userInfo.getNickname();
@@ -49,18 +46,34 @@ public class JwtLoginSuccessHandler implements org.springframework.security.web.
         String kakaoRefreshToken = client.getRefreshToken() != null ? client.getRefreshToken().getTokenValue() : null;
         Instant refreshTokenExpiresAt = client.getRefreshToken() != null ? client.getRefreshToken().getExpiresAt() : null;
 
+        UpsertKakaoUserDto dto = UpsertKakaoUserDto.createDto(kakaoId, nickname, profileImageUrl, kakaoAccessToken,
+                kakaoRefreshToken, accessTokenExpiresAt, refreshTokenExpiresAt);
 
-        UpsertKakaoUserDto dto = UpsertKakaoUserDto.createDto(kakaoId, nickname, profileImageUrl, kakaoAccessToken, kakaoRefreshToken, accessTokenExpiresAt, refreshTokenExpiresAt);
         String userId = userService.upsertKakaoUser(dto);
 
         String accessToken = jwtUtil.createAccessToken(userId);
+        addAccessTokenToCookie(response, accessToken);
+        response.sendRedirect(REDIRECT_URL);
+    }
+
+    private KakaoUserInfo getKakaoUserInfo(Authentication authentication) {
+        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+        Map<String, Object> attributes = oauth2User.getAttributes();
+        return objectMapper.convertValue(attributes, KakaoUserInfo.class);
+    }
+
+    private void addAccessTokenToCookie(HttpServletResponse response, String accessToken) {
         Cookie cookie = new Cookie("ACCESS_TOKEN", accessToken);
         cookie.setHttpOnly(true);
         cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge((int) Duration.ofMinutes(60).getSeconds());
-
         response.addCookie(cookie);
-        response.sendRedirect("http://localhost:3000/profile");
+    }
+
+    private OAuth2AuthorizedClient getOAuth2AuthorizedClient(OAuth2AuthenticationToken token) {
+        return oAuth2AuthorizedClientService.loadAuthorizedClient(
+                token.getAuthorizedClientRegistrationId(),
+                token.getName());
     }
 }
