849a98aac5ef88a3e0c27a113338cec158ee160f
Recionds <56007790+Recionds@users.noreply.github.com>
2025-11-14 13:41:16 +0900

    Refactor : 구글 로그인 토큰을 DB에 저장하여 로그인 상태가 유지되도록 변경

---
+++ src/main/java/org/dallyeo/matuabom/domain/GoogleOAuthClientEntity.java
@@ -0,0 +1,30 @@
+package org.dallyeo.matuabom.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+import java.util.Set;
+
+@Data
+@Document(collection = "google_oauth_clients")
+public class GoogleOAuthClientEntity {
+
+    @Id
+    private String id; // google:userId 형태로 저장
+
+    private String userId;
+    private String googleEmail;
+
+    private String accessToken;
+    private Instant accessTokenIssuedAt;
+    private Instant accessTokenExpiresAt;
+
+    private String refreshToken;
+    private Instant refreshTokenIssuedAt;
+
+    private Set<String> scopes;
+    private Instant updatedAt;
+}
+
