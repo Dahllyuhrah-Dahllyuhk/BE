c40fd3ebc5f2e17c182fc51b338f75cc3d39ab39
Daewon Jeong <daewon6905@gmail.com>
2025-11-20 11:32:13 +0900

    Refactor : 구글 동기화 영향 안가게 토큰 중복 검증 문제 수정

--- src/main/java/org/dallyeo/matuabom/controller/AuthController.java
+++ src/main/java/org/dallyeo/matuabom/controller/AuthController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.dallyeo.matuabom.domain.User;
 import org.dallyeo.matuabom.dto.MeDto;
 import org.dallyeo.matuabom.repository.UserRepository;
-import org.dallyeo.matuabom.security.CustomPrincipal;
 import org.dallyeo.matuabom.util.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,11 +21,11 @@ public class AuthController {
     private final UserRepository userRepository;
 
     @GetMapping("/api/auth/me")
-    public ResponseEntity<?> me(@AuthenticationPrincipal CustomPrincipal customPrincipal) {
-        if (customPrincipal == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
-        }
-        User user = userRepository.findById(customPrincipal.getUserId()).orElseThrow(() ->new UsernameNotFoundException("user not found"));
+    public ResponseEntity<?> me(@AuthenticationPrincipal String userId) {
+        if (userId == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("not authenticated");
+
+        User user = userRepository.findById(userId).orElseThrow(() ->new UsernameNotFoundException("user not found"));
         return ResponseEntity.ok().body(MeDto.createDto(user));
+
     }
 }
--- src/main/java/org/dallyeo/matuabom/controller/FriendController.java
+++ src/main/java/org/dallyeo/matuabom/controller/FriendController.java
@@ -6,7 +6,6 @@ import org.dallyeo.matuabom.domain.User;
 import org.dallyeo.matuabom.dto.AddByCodeRequest;
 import org.dallyeo.matuabom.dto.FriendDto;
 import org.dallyeo.matuabom.dto.InviteCodeResponse;
-import org.dallyeo.matuabom.security.CustomPrincipal;
 import org.dallyeo.matuabom.service.FriendService;
 import org.dallyeo.matuabom.service.InviteCodeService;
 import org.dallyeo.matuabom.util.JwtUtil;
@@ -31,16 +30,15 @@ public class FriendController {
 
 
     @GetMapping("/invite-code")
-    public ResponseEntity<InviteCodeResponse> getMyInviteCode(@AuthenticationPrincipal CustomPrincipal customPrincipal) {
-        InviteCode inviteCode = inviteCodeService.getOrCreateMyInviteCode(customPrincipal.getUserId());
-
+    public ResponseEntity<InviteCodeResponse> getMyInviteCode(@AuthenticationPrincipal String userId) {
+        InviteCode inviteCode = inviteCodeService.getOrCreateMyInviteCode(userId);
         return ResponseEntity.ok().body(new InviteCodeResponse(inviteCode.getOwnerUserId(), inviteCode.getCode()));
     }
 
     @PostMapping("/addFriend")
-    public ResponseEntity<?> addByCode(@AuthenticationPrincipal CustomPrincipal customPrincipal,@RequestBody AddByCodeRequest dto) {
+    public ResponseEntity<?> addByCode(@AuthenticationPrincipal String userId,@RequestBody AddByCodeRequest dto) {
         try {
-            User friend = friendService.addFriend(customPrincipal.getUserId(), dto.getCode());
+            User friend = friendService.addFriend(userId, dto.getCode());
             return ResponseEntity.ok().body(FriendDto.create(friend)); //친구 추가된 친구 유저의 정보 반환
         } catch (IllegalArgumentException e) {
             //초대코드가 없거나 잘못된 입력
@@ -61,8 +59,8 @@ public class FriendController {
     }
 
     @GetMapping
-    public ResponseEntity<List<FriendDto>> getMyFriends(@AuthenticationPrincipal CustomPrincipal customPrincipal) {
-        List<FriendDto> friends = friendService.getFriendsList(customPrincipal.getUserId())
+    public ResponseEntity<List<FriendDto>> getMyFriends(@AuthenticationPrincipal String userId) {
+        List<FriendDto> friends = friendService.getFriendsList(userId)
                 .stream()
                 .map(FriendDto::create)
                 .toList();
