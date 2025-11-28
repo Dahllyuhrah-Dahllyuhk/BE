98432e12757b0f3df8f7858b7ca34cd5539c65d3
Daewon Jeong <daewon6905@gmail.com>
2025-11-28 16:11:39 +0900

    rebase 전 커밋

--- src/main/java/org/dallyeo/matuabom/dto/MeDto.java
+++ src/main/java/org/dallyeo/matuabom/dto/MeDto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.dallyeo.matuabom.domain.User;
 
+import java.time.Instant;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,8 +14,9 @@ public class MeDto {
     private String id;
     private String nickname;
     private String profileImageUrl;
+    private Instant createdAt;
 
     public static MeDto createDto(User user) {
-        return new MeDto(user.getId(), user.getNickname(), user.getProfileImageUrl());
+        return new MeDto(user.getId(), user.getNickname(), user.getProfileImageUrl(),user.getCreatedAt());
     }
 }
