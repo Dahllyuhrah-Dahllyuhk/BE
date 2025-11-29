908752f4d3ad3ac21882343306cb00b773bbaa14
정대원 <daewon6905@gmail.com>
2025-11-29 12:47:12 +0900

    Bug : closed 상태인 모임도 집계하도록 수정 (#51)
    ## #️⃣ 연관된 이슈
    > 50
    
    ## 📝 작업 내용
    confirm에서 closed로 바뀐 모임도 집계하도록 수정했습니다.
    
    ### ✨ 스크린샷
    
    ### 💬 리뷰 요구사항

--- src/main/java/org/dallyeo/matuabom/repository/MeetingRepository.java
+++ src/main/java/org/dallyeo/matuabom/repository/MeetingRepository.java
@@ -27,6 +27,7 @@ public interface MeetingRepository extends MongoRepository<Meeting, String> {
     List<Meeting> searchByTitleAndDateRange(String userId, String keyword, String queryStart, String queryEnd);
 
     List<Meeting> findByStatusAndParticipantsUserId(String status, String userId);
+    List<Meeting> findByStatusOrStatusAndParticipantsUserId(String status1, String status2, String userId);
 
 
 }
